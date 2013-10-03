35d22c1e433521ee8a4b2166939f1a188da3b8f4
Andrew J Barbour <andy.barbour@gmail.com>
2013-10-03 00:00:00 +0000

    version 2.1-0

--- R/alpha_constants.R
+++ R/alpha_constants.R
@@ -1,5 +1,6 @@
-#' Calculate any constants depending on \eqn{\alpha}
+#' Calculate any constants depending on effective stress coefficient \eqn{\alpha}
 #' 
+#' @description
 #' This function accesses the appropriate method to calculate the
 #' \eqn{\alpha}-dependent constant associated with the choice of \code{c.type}.  
 #' There are currently four such constants, which correspond to
@@ -7,19 +8,23 @@
 #' 
 #' \emph{This function is not likely to be needed by the user.}
 #' 
+#' @details
+#' \subsection{What is \code{"alpha"}?}{
 #' The constant \eqn{\alpha} is a function of frequency \eqn{\omega} as well 
 #' as aquifer and well parameters; it is formally defined as
 #' \deqn{\alpha \equiv R_S \sqrt{\omega S / T}}
 #' where \eqn{S} is the storativity, \eqn{T} is the aquifer's effective
 #' transmissivity, and \eqn{R_S} is the radius of the screened portion
 #' of the well.
-#' 
-#' The various constants which may be calculated are
+#' }
+#' \subsection{What is calculated?}{
+#' The various constants which may be calculated with this function are
 #' \describe{
 #'   \item{\code{Phi}}{Given as \eqn{\Phi} in Eqn. 10}
 #'   \item{\code{Psi}}{Given as \eqn{\Psi} in Eqn. 11}
 #'   \item{\code{A}}{Given as \eqn{A_i, i=1,2} in Eqns. 18, 19}
-#'   \item{\code{Kel}}{\eqn{\mathcal{K}}, the complex Kelvin functions (see Abramowitz and Stegun, 1972)}
+#'   \item{\code{Kel}}{The complex Kelvin functions (see Abramowitz and Stegun, 1972)}
+#' }
 #' }
 #'
 #' @name alpha_constants
@@ -33,19 +38,10 @@
 #' \emph{as well as} any other \eqn{\alpha}-dependent constants 
 #' which are needed in the computation.
 #' 
-#' @author Andrew Barbour <andy.barbour@@gmail.com>
-#' 
-#' @references Kitagawa, Y., S. Itaba, N. Matsumoto, and N. Koisumi (2011),
-#' Frequency characteristics of the response of water pressure in a closed well to volumetric strain in the high-frequency domain,
-#' \emph{J. Geophys. Res.}, \strong{116}, B08301, doi:10.1029/2010JB007794.
-#' 
-#' @references \url{http://www.agu.org/pubs/crossref/2011/2010JB007794.shtml}
+#' @author A. J. Barbour <andy.barbour@@gmail.com>
 #'
-#' @references Abramowitz, M. and Stegun, I. A. (Eds.). "Kelvin Functions." 
-#' \eqn{\S 9.9} in Handbook of Mathematical Functions with Formulas, Graphs, 
-#' and Mathematical Tables, 9th printing. New York: Dover, pp. 379-381, 1972.
-#'
-#' @seealso \code{\link{omega_constants}}, \code{\link{well_response}}, \pkg{kelvin}
+#' @seealso \code{\link{omega_constants}}, \code{\link{well_response}}
+#' @family ConstantsCalculators
 #' 
 #' @examples
 #' alpha_constants() # kelvin::Keir gives warning
@@ -54,9 +50,7 @@
 alpha_constants <-
 function(alpha=0, c.type=c("Phi","Psi","A","Kel")) UseMethod("alpha_constants")
 
-# @return \code{NULL}
 #' @rdname alpha_constants
-#' @docType methods
 #' @method alpha_constants default
 #' @S3method alpha_constants default
 alpha_constants.default <-
@@ -88,7 +82,6 @@ alpha_constants.default <-
         return(toret)
     } # end .ac_A
     .ac_Kel.default <- function(alpha){
-        require(kelvin)
         # nu is order of Kelvin function
         Kel <- kelvin::Keir(alpha, nu.=0, nSeq.=2, return.list=FALSE)
         # returns K0,K1 (complex matrix)
@@ -125,6 +118,3 @@ alpha_constants.default <-
     toret <- c.calc(alpha)
     return(toret)
   }
-
-
-#
--- R/well_response.R
+++ R/well_response.R
@@ -1,59 +1,65 @@
-#' Calculate the pressure/strain response spectrum for given formation properties
+#' Spectral response for a sealed well
 #'
-#' This is the primary function which calculates the theoretical, complex
-#' well response, namely \strong{Equation 17} in Kitagawa et al (2011).
-#' The results, however, are expressed as amplitude and phase.
+#' This is the primary function to calculate the response
+#' for a sealed well.
 #' 
-#' \strong{The response depends strongly on the physical properties
-#' given.
-#' Default values are assumed where resonable, mostly that the pore-fluid
-#' is water, but considerable care should be invested in the choice of
-#' parameters, unless the function is used in an optimization scheme.}
+#' @details
+#' The response depends strongly on the physical properties
+#' given. Default values are assumed where reasonable--for instance, 
+#' the pore-fluid is assumed to be water--but considerable care 
+#' should be invested in the choice of
+#' parameters, unless the function is used in an optimization scheme.
 #' 
 #' Assumed values are:
-#' \describe{
-#' \item{\code{Avs.}}{1}
-#' \item{\code{Aw.}}{1}
-#' \item{\code{rho.}}{\eqn{1000 [kg/m^3]}, the density of water}
-#' \item{\code{Kf.}}{\eqn{2.2e9 [Pa]}, the bulk modulus of water}
-#' \item{\code{grav.}}{\eqn{9.81 [m/s^2]}, average gravitational force on Earth}
+#' \tabular{rlrl}{
+#' \code{Avs}  \tab 1 \tab \tab amplification factor for volumetric strain\cr
+#' \code{Aw}   \tab 1 \tab \tab amplification factor for water well\cr
 #' }
 #' 
-#' Note that Skempton's coefficient, \code{B.}, is bounded inclusively
-#' within \eqn{[0,1]}; an error is thrown if it's not.
+#' The responses returned here are,
+#' effectively, the amplification of water levels in a well, relative to 
+#' the aquifer strain; or
+#' \deqn{Z = \frac{z}{\epsilon} \equiv \frac{p}{\rho g \epsilon}}
+#' If \code{as.pressure=TRUE}, then the responses are scaled by
+#' \code{rho*grav} so that they represent water pressure relative to
+#' aquifer strain:
+#' \deqn{Z = \frac{p}{\epsilon}}
+#' 
+#' Not all parameters need to be given, but should be.  
+#' For example, if
+#' either \code{rho} or \code{grav} are not specified, they
+#' are taken from \code{\link{constants}}.
+#' \emph{Parameters which do not end in \code{.} do
+#' not need to be specified (they may be excluded); if
+#' they are missing, warnings will be thrown.}
 #'
 #' @name well_response
 #' @export
 #' 
-#' @param omega  frequency,  (see freq.units)
+#' @param omega  frequency, (see \code{freq.units})
 #' @param T.     effective aquifer transmissivity \eqn{[m^2/s]}
 #' @param S.     well storativity,  \eqn{[unitless]}
 #' @param Vw.    well volume,	 \eqn{[m^3]}
 #' @param Rs.    radius of screened portion,  \eqn{[m]}
 #' @param Ku.    undrained bulk modulus,  \eqn{[Pa]}
 #' @param B.     Skempton's coefficient,  \eqn{[unitless, bounded]}
-#' @param Avs.   amplification factor for volumetric strain \eqn{E_{kk,obs}/E_{kk}},  \eqn{[]}
-#' @param Aw.    amplification factor of well volume change for \eqn{E_{kk}},  \eqn{[]}
-#' @param rho.   fluid density \eqn{[kg/m^3]}
-#' @param Kf.    bulk modulus of fluid,  \eqn{[Pa]}
-#' @param grav.  local gravitational acceleration \eqn{[m/s^2]}
-#' @param freq.units  set what the units of frequency (omega) are: \code{"rad_per_sec"} (default, \code{NULL}), or \code{"Hz"}
+#' @param Avs   amplification factor for volumetric strain \eqn{E_{kk,obs}/E_{kk}},  \eqn{[]}
+#' @param Aw    amplification factor of well volume change for \eqn{E_{kk}},  \eqn{[]}
+#' @param rho   fluid density \eqn{[kg/m^3]}
+#' @param Kf    bulk modulus of fluid,  \eqn{[Pa]}
+#' @param grav  local gravitational acceleration \eqn{[m/s^2]}
+#' @param freq.units  set the units of \code{omega}
+#' @param as.pressure logical; should the response for water pressure? (default is water heights)
 #'
-#' @return Matrix with three columns: radial frequency, amplitude, and phase 
-#' [\eqn{\omega}), \eqn{A_\alpha (\omega)}, \eqn{\Phi_\alpha (\omega)}]
-#' where the units of \eqn{\omega} will be radians per second,
-#' \eqn{A_\alpha (\omega)} in meters per strain, 
-#' and \eqn{\Phi_\alpha (\omega)} in radians.
+#' @return An object with class 'wrsp'
 #' 
-#' @author Andrew Barbour <andy.barbour@@gmail.com>
-#' 
-#' @references Kitagawa, Y., S. Itaba, N. Matsumoto, and N. Koisumi (2011),
-#' Frequency characteristics of the response of water pressure in a closed well to volumetric strain in the high-frequency domain,
-#' \emph{J. Geophys. Res.}, \strong{116}, B08301, doi:10.1029/2010JB007794.
-#' 
-#' @references \url{http://www.agu.org/pubs/crossref/2011/2010JB007794.shtml}
+#' @author A. J. Barbour <andy.barbour@@gmail.com>
 #'
-#' @seealso \code{\link{sensing_volume}}, \code{\link{kitplot}}
+#' @seealso 
+#' \code{\link{wrsp-methods}} for a description of the class 'wrsp' and its methods,
+#' \code{\link{sensing_volume}} to easily estimate the volume \code{Vw.}, and
+#' \code{\link{kitagawa-package}} for references and more background.
+#' @family WellResponseFunctions
 #' 
 #' @examples
 #' #### dummy example
@@ -74,95 +80,95 @@
 #' Frqs <- 10**seq.int(from=-4,to=0,by=0.1) # log10-space
 #' head(Rsp <- well_response(omega=Frqs, T.=1e-6, S.=1e-5, 
 #' Vw.=Volw, Rs.=Rs, Ku.=40e9, B.=0.2, freq.units="Hz"))
-#' #
-#' kitplot(Rsp)
+#' 
+#' # Access plot.wrsp:
+#' plot(Rsp)
 #'
-well_response <-
-function(omega,
-         T., S., Vw., Rs., Ku., B.,
-         Avs.=1,
-         Aw.=1,
-         rho.=1000, 
-         Kf.=2.2e9,
-         grav.=9.81,
-         freq.units=NULL) UseMethod("well_response")
+well_response <- function(omega, T., S., Vw., Rs., Ku., B., 
+                          Avs, Aw, rho, Kf, grav,
+                          freq.units=c("rad_per_sec","Hz"),
+                          as.pressure=TRUE) UseMethod("well_response")
 
-# @return \code{NULL}
 #' @rdname well_response
-#' @docType methods
 #' @method well_response default
 #' @S3method well_response default
-well_response.default <-
-  function(omega,
-           T., S., Vw., Rs., Ku., B.,
-           Avs.=1,
-           Aw.=1,
-           rho.=1000, 
-           Kf.=2.2e9,
-           grav.=9.81,
-           freq.units=NULL){
-    #
+well_response.default <- function(omega, T., S., Vw., Rs., Ku., B., 
+                                  Avs, Aw, rho, Kf, grav,
+                                  freq.units=c("rad_per_sec","Hz"),
+                                  as.pressure=TRUE){
     # Enforce units of omega to be radians/sec
-    fc <- switch(match.arg(freq.units, c("rad_per_sec","Hz")),
-                 rad_per_sec=1,
-                 Hz=2*pi
-    )
+    freq.units <- match.arg(freq.units)
+    fc <- switch(freq.units, rad_per_sec=1, Hz=2*pi)
     omega <- fc*omega
+    #
+    # Setup constants
+    defA <- 1
+    if (missing(Avs)){
+      Avs <- defA
+    }
+    if (missing(Aw)){
+      Aw <- defA
+    }
+    const <- kitagawa::constants(FALSE)
+    if (missing(rho)){
+      rho <- const$water$density
+    }
+    if (missing(Kf)){
+      Kf <- const$water$bulkmod
+    }
+    if (missing(grav)){
+      grav <- const$gravity
+    }
+    rhog <- rho*grav
     #
     # Alpha function
     Alpha. <- omega_constants(omega, c.type="alpha", S.=S., T.=T., Rs.=Rs.)
-    # A1, and A2 (functions of Phi and Psi, calculated internally)
+    #   A1, and A2 (functions of Phi and Psi, calculated internally)
     Amat <- alpha_constants(Alpha., c.type="A")
     stopifnot(ncol(Amat)==7)
-    rm(Alpha.)  # cleanup
     #  A1,2 are in Mod(A.[,6:7]) 
     A12 <- matrix(Mod(Amat[,6:7]),ncol=2)  # is complex, but imag is zero, so == abs
     stopifnot(ncol(A12)==2)
-    rm(Amat)    # cleanup
     A1 <- A12[,1]
     A2 <- A12[,2]
-    rm(A12)     # cleanup
     #
-    # prevent duplicate calculations
-    rhog <- rho. * grav.
-    #print(summary(omega))
     TVFRG <- 2 * pi * T. / omega / Vw. / rhog
     #
-    #check B: it's bounded between [0,1]
-    stopifnot((B.>=0) & (B.<=1))
+    #check Skemptons coefficient: it should be bound to [0,1]
+    .in0to1(B.)
     #
-    # calculate amp and phase of response
+    XX. <- Ku. * B. / Aw * TVFRG - A2
+    rNum. <- XX. * XX. + A1 * A1
     #
-    tmpd. <- Ku. * B. / Aw. * TVFRG - A2
-    rNum. <- tmpd. * tmpd. + A1 * A1
-    rm(tmpd.)
-    tmpd. <- Kf. * TVFRG  -  A2
-    rDen. <- tmpd. * tmpd. + A1 * A1
-    rm(tmpd.)
+    YY. <- Kf * TVFRG  -  A2
+    rDen. <- YY. * YY. + A1 * A1
     ##
     ## complex response EQ 17
-    ## cNum <- complex(real=(Ku. * B. / Aw. * TVFRG - A2), imaginary=A1)
-    ## cDen <- complex(real=(Kf. * TVFRG  -  A2), imaginary=A1)
-    ## cResp <- -1 * Kf. * Aw. / Avs. / rhog * cNum / cDen
-    ## amplitude
-    ## Amp. <- Mod(cResp)
-    ## Phs. <- Arg(cResp)
-    ##
-    ## amplitude, Kitagawa equation 20
-    ##
-    Amp. <- Kf. * Aw. / Avs. / rhog * sqrt(rNum. / rDen.)
-    ##
-    ## phase, Kitagawa equation 21
-    ##
-    Y. <- (Kf. - Ku. * B. / Aw.) * TVFRG * A1
-    X. <- (Ku. * B. / Aw. * TVFRG - A2) * (Kf. * TVFRG - A2) + A1 * A1
-    Phs. <- atan2(-1*Y.,-1*X.)
+    cNum <- complex(real=(Ku. * B. / Aw * TVFRG - A2), imaginary=A1)
+    cDen <- complex(real=(Kf * TVFRG  -  A2), imaginary=A1)
     #
-    # params?
-    # attributes?
-    # message?
-    #
-    # return results
-    toret <- cbind(omega, Amp., Phs.)
+    # z / epsilon == p / epsilon * rho * g
+    # but is there a mistake?
+    # / rhog
+    wellresp <- -1 * Kf * Aw / Avs  *  cNum / cDen 
+    ##
+    # optionally scale to p/epsilon
+    rhog <- ifelse(as.pressure, rhog, 1)
+    Zunits <- ifelse(as.pressure, "P/E", "Z/E")
+    wellresp <- wellresp * rhog
+    ##
+    omega <- omega/fc
+    toret <- list(Aquifer=list(Transmiss=T., Storativ=S., Diffusiv=T./S.),
+                  Well=list(Volume=Vw., ScreenRad=Rs.),
+                  Solid=list(BulkModU=Ku.,Skemp=B.),
+                  Fluid=list(BulkMod=Kf, Density=rho),
+                  Amplification=list(Ekk=Avs, Well=Aw),
+                  Omega=list(Units=freq.units),
+                  Gravity=grav,
+                  Model=list(Model="kitagwawa", Pressure=as.pressure),
+                  Response=cbind(omega=omega, wellresp=wellresp),
+                  Response.units=Zunits
+    )
+    class(toret) <- "wrsp"
     return(toret)
-  }
+}
